d9147fa0d52ced1516f3f13539329cc800481073
Matt Hays <mdhays1995@gmail.com>
2016-12-09 15:30:12 -0600

    Added Firebase and Bootstrap to app.

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCmdDzyQfIwPhY7CPnA7VkCSvYiwGsL6NE",
+    authDomain: "angular-demo-1da34.firebaseapp.com",
+    databaseURL: "https://angular-demo-1da34.firebaseio.com",
+    storageBucket: "angular-demo-1da34.appspot.com",
+    messagingSenderId: "800533843604"
+  };
+  firebase.initializeApp(config);
